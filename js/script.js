515e61821a9c2aa67e651cceb999de62e61d1df1
Вася <kiriljuk.vasja@gmail.com>
2017-10-04 18:23:26 +0300

    big logo

--- js/script.js
+++ js/script.js
@@ -8,4 +8,10 @@ $(document).ready(function () {
         $('.top-slides-content-elements').addClass('has-active-child');
     });
 
+    $('.top-slide-content-element-preview-block .content-block .close-button .plus-icon').on('click', function (e) {
+        e.stopPropagation();
+        $('.top-slide-content-element').removeClass('active');
+        $('.top-slides-content-elements').removeClass('has-active-child');
+    });
+
 });
