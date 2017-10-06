6273ea1d405bcb78271c00dbc9859f7465e81fe8
Вася <kiriljuk.vasja@gmail.com>
2017-10-06 19:05:45 +0300

    fix slider and bg-color of item

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-jQuery(document).ready(function( $ ) {
-    $('#my-slider').sliderPro({
-        width: 356,
-        height: 430,
-        autoplay: false,
-        orientation: 'vertical',
-        loop: false,
-        arrows: true,
-        buttons: false,
-        thumbnailsPosition: 'right',
-        thumbnailPointer: false,
-        thumbnailArrows: true,
-        thumbnailWidth: 93,
-        thumbnailHeight: 113
-    });
-});
-
 $(document).ready(function () {
 
     //top slides content tabs
@@ -23,13 +6,25 @@ $(document).ready(function () {
         $('.top-slide-content-element').removeClass('active');
         $(this).addClass('active');
         $('.top-slides-content-elements').addClass('has-active-child');
+        $('.slider-megasuperpuper').sliderPro({
+            width: 356,
+            height: 430,
+            autoplay: false,
+            orientation: 'vertical',
+            loop: false,
+            arrows: true,
+            buttons: false,
+            thumbnailsPosition: 'right',
+            thumbnailPointer: false,
+            thumbnailArrows: true,
+            thumbnailWidth: 93,
+            thumbnailHeight: 113
+        });
     });
 
     $('.top-slide-content-element-preview-block .content-block .close-button .plus-icon').on('click', function (e) {
         e.stopPropagation();
-        setTimeout(function () {
-            $('.top-slide-content-element').removeClass('active');
-        }, 700);
+        $('.top-slide-content-element').removeClass('active');
         $('.top-slides-content-elements').removeClass('has-active-child');
     });
 
