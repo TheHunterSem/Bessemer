38711b01be1bcdce5e034d5e536b68613433f5e8
Вася <kiriljuk.vasja@gmail.com>
2017-10-12 17:05:01 +0300

    transition popup

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
 
     $('.sp-image-link').magnificPopup({
-        type: 'image'
+        type: 'image',
+        // Delay in milliseconds before popup is removed
+        removalDelay: 300,
+
+        // Class that is added to popup wrapper and background
+        // make it unique to apply your CSS animations just to this exact popup
+        mainClass: 'mfp-fade'
     });
 
     $('.slider-pro').sliderPro({
